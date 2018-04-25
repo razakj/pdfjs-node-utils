d7dcaa511415e4513d134585411c074056459bf2
Jakub Razak <methodcz@gmail.com>
2018-04-25 19:43:50 +0800

    Fix

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const workers   = require('./src/workers');
 module.exports = function({
     noOfWorkers = 1
 }) {
-    if(noOfWorkers < 1) {
-        return require('./src');
-    } else {
-        return workers.init(noOfWorkers);
-    }
+    // if(noOfWorkers < 1) {
+    //     return require('./src');
+    // } else {
+    //     return workers.init(noOfWorkers);
+    // }
+    return require('./src');
 };
