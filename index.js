68d22560c27e75a7d62b86a48976d94850f50f6b
Jakub Razak <methodcz@gmail.com>
2018-04-25 20:05:13 +0800

    Fix #2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const workers   = require('./src/workers');
 
 module.exports = function({
     noOfWorkers = 1
-}) {
+} = {}) {
     // if(noOfWorkers < 1) {
     //     return require('./src');
     // } else {
